4b27873eba231ba1f69cb2c03ac7dd8c92e04c97
JoshJensen <107435158+JoshJensen86@users.noreply.github.com>
2022-07-20 13:55:14 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 
 app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/main.js'))
+    res.sendFile(path.join(__dirname, '../index.js'))
   })
 
   
